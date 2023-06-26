947e4c044eb1c5e47349c4c18b4e288e98fe0f51
Victor <9507630@qq.com>
2023-06-26 15:21:42 +0800

    Fixed router import issue

--- routers/api.js
+++ routers/api.js
@@ -1,5 +1,10 @@
 import { Router } from 'express';
-import { httpCodes } from '../modules/http-manager.js';
+import { VARS } from '../modules/vars.js';
+import { httpError, httpCodes, httpMessages } from '../modules/http-manager.js';
+import jwt from '../modules/jwt.js';
+import { Logger } from '../modules/logger.js';
+const { logger } = Logger('token');
+
 const publicRouter = Router();
 const publicPath = '/api';
 
@@ -7,7 +12,7 @@ const publicPath = '/api';
  * API endpoint
  */
 publicRouter.get('/', async (req, res, next) => {
-  return res.send({ status: httpCodes.OK });
+  return res.send({ status: httpCodes.OK, message: 'API endpoint' });
 });
 
 /**
@@ -16,6 +21,54 @@ publicRouter.get('/', async (req, res, next) => {
 publicRouter.post('/status/report/:id', async (req, res, next) => {
   const { id } = req.params;
   return res.send({ status: httpCodes.OK, id, message: 'This API is only used for serving Status Report Test. We do not store any data you post here.' });
+});
+
+/**
+ * Generate JWT
+ */
+publicRouter.get('/token/generate', async (req, res, next) => {
+  try {
+    const { sub, exp = VARS.JWT_DURATION } = req.headers;
+    logger.debug(sub, exp);
+    const expTime = Number(exp);
+    const token = await jwt.generateToken(sub, expTime === Number.NaN ? exp : expTime);
+    return res.send({ token });
+  }
+  catch(error) {
+    next(httpError(httpCodes.BAD_REQUEST, httpMessages.BAD_REQUEST, error));
+  }
+});
+
+/**
+ * Generate JWT with extra fields
+ */
+publicRouter.post('/token/generate', async (req, res) => {
+  const { sub, exp = VARS.JWT_DURATION } = req.headers;
+  logger.debug(sub, exp);
+  const expTime = Number(exp);
+  const token = await jwt.generateToken(sub, expTime === Number.NaN ? exp : expTime, req.body ?? {});
+  res.send({ token });
+});
+
+/**
+ * Verify JWT
+ */
+publicRouter.get('/token/verify', async (req, res, next) => {
+  try {
+    const { token = '' } = req.headers;
+    const result = await jwt.verifyToken(token);
+    res.send(result.payload);
+  }
+  catch (error) {
+    next(httpError(httpCodes.BAD_REQUEST, httpMessages.INVALID_TOKEN, error));
+  }
+});
+
+/**
+ * Discover JWT
+ */
+publicRouter.get('/token/discovery', async (_req, res) => {
+  res.send({ issuer: VARS.JWT_ISSUER, audience: VARS.JWT_AUDIENCE, url: VARS.JWT_ENDPOINT_URL });
 });
 
 export {
