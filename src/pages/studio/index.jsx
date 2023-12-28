3a536259bee7cd898ab4663312b0415e81e27a9a
Victor <9507630@qq.com>
2023-12-29 07:17:47 +0800

    Adjusted styles

--- src/pages/studio/index.jsx
+++ src/pages/studio/index.jsx
@@ -11,23 +11,23 @@ import CircularProgress from '@mui/material/CircularProgress';
 export default function Studio({ translate }) {
   const [ data, setData ] = useState('');
   const [ status, setStatus ] = useState(false);
-  const [ isLoading, setIsLoading ] = useState(true);
+  const [ isLoading, setIsLoading ] = useState(false);
 
-  useEffect(() => {
-    (async() => {
-      setIsLoading(true);
-      const result = await studioService.getDeviceProperties('6c6e0a2cc4d07963ad2nvh', { codes: 'switch_1' });
-      if(result && result.properties && result.properties.length > 0) {
-        setStatus(result.properties[0].value);
-        setData(JSON.stringify(result.properties[0]));
-      }
-      setIsLoading(false);
-    })();
-  }, []);
+  // useEffect(() => {
+  //   (async() => {
+  //     setIsLoading(true);
+  //     const result = await studioService.getDeviceProperties('6c6e0a2cc4d07963ad2nvh', { codes: 'switch_1' });
+  //     if(result && result.properties && result.properties.length > 0) {
+  //       setStatus(result.properties[0].value);
+  //       setData(JSON.stringify(result.properties[0]));
+  //     }
+  //     setIsLoading(false);
+  //   })();
+  // }, []);
 
   return <div className={styles['studio']}>
     <div className={styles['main']}>
-      <Card variant="outlined" sx={{ maxWidth: 600, alignItems: 'center' }}>
+      <Card variant="outlined" sx={{ alignItems: 'center', width: '100%', maxWidth: '500px', minWidth: '300px', boxShadow: 'var(--box-shadow-global)' }}>
         <Typography level="h1">绣声坊</Typography>
         <Typography level="h2" fontSize="xl" sx={{ mb: 0.5 }}>
           点亮琴房
@@ -37,8 +37,8 @@ export default function Studio({ translate }) {
         </Typography>
         <div className={styles['functions']}>
           <IconButton sx={{
-            width: '80%',
-            height: '80%',
+            width: '10rem',
+            height: '10rem',
             borderRadius: '50%',
             boxShadow: '3px 7px 5px #bbb'
           }} disabled={isLoading} variant='solid' color={status ? 'danger' : 'success'} onClick={async evt => {
@@ -51,14 +51,14 @@ export default function Studio({ translate }) {
             }
             setIsLoading(false);
           }} size='lg'>{ isLoading ? <CircularProgress size='lg' sx={{
-            width: '50%',
-            height: '50%',
+            width: '5rem',
+            height: '5rem',
           }} /> : status ? <FlashOffIcon sx={{
-            width: '50%',
-            height: '50%',
+            width: '5rem',
+            height: '5rem',
           }} /> : <FlashOnIcon sx={{
-            width: '50%',
-            height: '50%',
+            width: '5rem',
+            height: '5rem',
           }} /> }</IconButton>
           {/* <Button variant='soft' onClick={async evt => {
             setIsLoading(true);
