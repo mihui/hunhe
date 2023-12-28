2ce1ec07b72ea2992f621cbaf6345a9d6cbd5ef6
Victor <9507630@qq.com>
2023-12-29 01:02:20 +0800

    Just for testing

--- src/pages/studio/index.jsx
+++ src/pages/studio/index.jsx
@@ -2,17 +2,16 @@
 import { studioService } from '@/components/services/studio';
 import styles from '@/styles/studio.module.scss';
 
-import { Button } from "@mui/joy";
+import { Button, Card, IconButton, Typography } from "@mui/joy";
 import { useEffect, useState } from 'react';
+import FlashOnIcon from '@mui/icons-material/FlashOn';
+import FlashOffIcon from '@mui/icons-material/FlashOff';
+import CircularProgress from '@mui/material/CircularProgress';
 
 export default function Studio({ translate }) {
   const [ data, setData ] = useState('');
   const [ status, setStatus ] = useState(false);
   const [ isLoading, setIsLoading ] = useState(true);
-
-  useEffect(() => {
-    console.log('data->', data);
-  }, [data]);
 
   useEffect(() => {
     (async() => {
@@ -28,28 +27,53 @@ export default function Studio({ translate }) {
 
   return <div className={styles['studio']}>
     <div className={styles['main']}>
-      <div className={styles['functions']}>
-        <Button disabled={isLoading} onClick={async evt => {
-          setIsLoading(true);
-          const newStatus = !status;
-          const result = await studioService.setDeviceProperties('6c6e0a2cc4d07963ad2nvh', { switch_1: newStatus });
-          if(result) {
-            setData(JSON.stringify(result));
-            setStatus(newStatus);
-          }
-          setIsLoading(false);
-        }} size='lg'>{ isLoading ? '请稍候' : status ? '关灯' : '开灯' }</Button>
-        {/* <Button variant='soft' onClick={async evt => {
-          setIsLoading(true);
-          const result = await studioService.deviceInfo('6c6e0a2cc4d07963ad2nvh');
-          if(result) {
-            setData(JSON.stringify(result));
-          }
-          setIsLoading(false);
-        }} size='lg'>
-          { isLoading ? '请稍候' : '获取设备信息' }
-        </Button> */}
-      </div>
+      <Card variant="outlined" sx={{ maxWidth: 600, alignItems: 'center' }}>
+        <Typography level="h1">绣声坊</Typography>
+        <Typography level="h2" fontSize="xl" sx={{ mb: 0.5 }}>
+          点亮琴房
+        </Typography>
+        <Typography>
+          扫码支付，成功后即可开门练琴。
+        </Typography>
+        <div className={styles['functions']}>
+          <IconButton sx={{
+            width: '80%',
+            height: '80%',
+            borderRadius: '50%',
+            boxShadow: '3px 7px 5px #bbb'
+          }} disabled={isLoading} variant='solid' color={status ? 'danger' : 'success'} onClick={async evt => {
+            setIsLoading(true);
+            const newStatus = !status;
+            const result = await studioService.setDeviceProperties('6c6e0a2cc4d07963ad2nvh', { switch_1: newStatus });
+            if(result) {
+              setData(JSON.stringify(result));
+              setStatus(newStatus);
+            }
+            setIsLoading(false);
+          }} size='lg'>{ isLoading ? <CircularProgress size='lg' sx={{
+            width: '50%',
+            height: '50%',
+          }} /> : status ? <FlashOffIcon sx={{
+            width: '50%',
+            height: '50%',
+          }} /> : <FlashOnIcon sx={{
+            width: '50%',
+            height: '50%',
+          }} /> }</IconButton>
+          {/* <Button variant='soft' onClick={async evt => {
+            setIsLoading(true);
+            const result = await studioService.deviceInfo('6c6e0a2cc4d07963ad2nvh');
+            if(result) {
+              setData(JSON.stringify(result));
+            }
+            setIsLoading(false);
+          }} size='lg'>
+            { isLoading ? '请稍候' : '获取设备信息' }
+          </Button> */}
+        </div>
+      </Card>
+
+
     </div>
   </div>;
 }
