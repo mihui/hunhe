cc8759e16fc828f4b562979f933ed6ff3955a1ed
Victor <9507630@qq.com>
2023-12-29 13:13:30 +0800

    Changed default host name

--- server/modules/vars.js
+++ server/modules/vars.js
@@ -100,7 +100,7 @@ const VARS = {
   IOT_API_ENDPOINT: process.env.IOT_API_ENDPOINT ?? 'https://openapi.tuyacn.com',
   IOT_CLIENT_ID: process.env.IOT_CLIENT_ID ?? '',
   IOT_CLIENT_SECRET: process.env.IOT_CLIENT_SECRET ?? '',
-  REMOTE_API_ENDPOINT: process.env.REMOTE_API_ENDPOINT ?? 'https://express-ab8i-86983-7-1323459498.sh.run.tcloudbase.com',
+  REMOTE_API_ENDPOINT: process.env.REMOTE_API_ENDPOINT ?? 'https://xiushengfang-86983-7-1323459498.sh.run.tcloudbase.com',
 };
 
 BigInt.prototype.toJSON = function() {
